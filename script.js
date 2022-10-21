35c41054d46b144649654d4a41a892ae9435a3f5
emmanuelsolis <emma.ruso@outlook.com>
2022-10-21 14:24:37 -0500

    Fetching Ispiration quotes API & adding eventListeners to buttons

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('Testing')
+const quoteConatiner = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+//show new quote
+function newQuote() {
+    // pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    }else {
+        authorText.textContent = quote.author;
+    }
+    //Check quote length to determine styling
+    if(quote.text.lenght > 120) {
+        quoteText.classList.add('long-quote');
+    }else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    }
+
+//get quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch (apiUrl);
+        apiQuotes = await response.json()
+        newQuote();
+    }catch(err) {
+        //catch error here
+
+    }
+}
+// Tweet Quote 
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+//Event Listeners adding event listeners to buttons
+twitterBtn.addEventListener('click', tweetQuote);
+getQuotes()
